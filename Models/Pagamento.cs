5cc0386677dbab7c44964a56b6e7f320c42b32cd
DanielSantini <danielsantini36@gmail.com>
2024-02-26 16:57:26 +0100

    Aggiungere i file di progetto.

---
+++ Models/Pagamento.cs
@@ -0,0 +1,11 @@
+﻿namespace AziendaEdile.Models
+{
+    public class Pagamento
+    {
+        public int Id { get; set; }
+        public int DipendenteId { get; set; }
+        public DateTime Periodo { get; set; }
+        public decimal Ammontare { get; set; }
+        public bool Stipendio { get; set; }
+    }
+}
